dec11dff50d926ee194801af54cd248b33fed965
ezha0 <ezhao516@gmail.com>
2022-11-28 17:52:59 -0500

    drag and drop?

--- game.js
+++ game.js
@@ -1,51 +1,25 @@
-function startGame() {
-      myGameArea.start();
-    }
-    
-    var myGameArea = {
-      canvas : document.createElement("canvas"),
-      start : function() {
-        this.canvas.width = 1280;
-        this.canvas.height = 720;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      }
-    }
+var dragValue;
 
-var myGamePiece;
-
-function startGame() {
-    myGameArea.start();
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece = function(event) {
-        myGamePiece.style.position = 'absolute';
-        myGamePiece.zIndex = 1000;
-        document.body.append(myGamePiece);
-        function moveAt(pageX, pageY) {
-            myGamePiece.style.left = pageX - myGamePiece.offsetWidth / 2 + 'px';
-            myGamePiece.style.top = pageY - myGamePiece.offsetHeight / 2 + 'px';
-            moveAt(event.pageX, event.pageY);
-            function onMouseMove(event) {
-                moveAt(event.pageX, event.pageY);
-            }
-            document.addEventListener('mousemove', onMouseMove);
-            myGamePiece.onmouseup = function() {
-                document.removeEventListener('mousemove', onMouseMove);
-                myGamePiece.onmouseup = null;
-            }
-            myGamePiece.ondragstart = function() {
-                return false;
-            }
-        }
+function move(id) {
+    var element = document.getElementById("thing");
+    element.style.position = "absolute";
+    element.onmousedown = function() {
+        dragValue = element;
     }
 }
+document.onmousemove = function(e) {
+    var x = e.pageX;
+    var y = e.pageY;
 
-function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;    
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    dragValue.style.left = x + "px";
+    dragValue.style.top = y + "px";
+
 }
+
+document.onmouseup = function(e){
+    dragValue = null;
+}
+function startGame() {
+    move("thing");
+}
+
