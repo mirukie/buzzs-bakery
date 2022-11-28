50d19fa8988ab6f2df0d7b74479c4b1b63fcea29
ezha0 <ezhao516@gmail.com>
2022-11-28 15:57:32 -0500

    separate js and html code

--- game.js
+++ game.js
@@ -11,3 +11,20 @@ function startGame() {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
       }
     }
+
+var myGamePiece;
+
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
