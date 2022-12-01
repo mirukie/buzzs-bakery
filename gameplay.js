c374710a5cbd512b34d8763444c42a13d2b29173
Emmaly Nguyen <56703411+emmalynguyen@users.noreply.github.com>
2022-12-01 14:47:35 -0500

    Update gameplay.js

--- gameplay.js
+++ gameplay.js
@@ -1,16 +1,6 @@
-function leftArrowPressed() {
-    var element = document.getElementById("image1");
-    element.style.left = parseInt(element.style.left) - 20 + 'px';
-}
-
 function rightArrowPressed() {
     var element = document.getElementById("image1");
     element.style.left = parseInt(element.style.left) + 20 + 'px';
-}
-
-function upArrowPressed() {
-    var element = document.getElementById("image1");
-    element.style.top = parseInt(element.style.top) - 20 + 'px';
 }
 
 function downArrowPressed() {
